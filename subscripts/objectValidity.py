8db246b00bd4c2e1559d1c8b4df44b42bb318d27
Pratik Deo <pratikdeo78@gmail.com>
2020-02-12 22:30:58 -0500

    Added methods calling User stories 1 and 10

--- subscripts/objectValidity.py
+++ subscripts/objectValidity.py
@@ -1,6 +1,9 @@
 # This function will call all the user story functions (from userStories folder's scripts)
 # Returns true only if every story checks out, or returns false
-
+from subscripts.userStories.UserStories_Pratik_Deo import DatebeforeCurrentDate
+from subscripts.userStories.UserStories_Pratik_Deo import MarriageAfter14
 
 def objectvalid(indi, fam):
+    DatebeforeCurrentDate(indi,fam)
+    MarriageAfter14(indi,fam)
     return True
