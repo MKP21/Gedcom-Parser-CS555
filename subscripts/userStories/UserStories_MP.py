c2fa46696e7b24352abc9b4e0d3a7c8deff2810a
MKP21 <meetknpatel2@gmail.com>
2020-02-25 12:23:45 -0500

    updated UserStories_MP

--- subscripts/userStories/UserStories_MP.py
+++ subscripts/userStories/UserStories_MP.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 # Birth before death
 def us2(indi, fam):
-    print("User Story 2 Running")
+    print("User Story 2 - Birth before death, Running")
     for person in indi:
         m = person['DEAT']
         if person['DEAT'] == 'NA':
@@ -13,3 +13,25 @@ def us2(indi, fam):
             print(f"{person['INDI']} {person['NAME']} were born before they died")
 
     print("User Story 2 Completed")
+    return True
+
+
+# Birth before marriage of parent
+def us8(indi, fam):
+    print("User Story 8 - Birth before marriage of parent, Running")
+    for family in fam:
+        marriagedate = family["MARR"]
+        for child in family["CHIL"]:
+            childobj = next((item for item in indi if item["INDI"] == child), False)
+
+            if childobj and childobj["BIRT"]>marriagedate:
+                continue
+            elif childobj["BIRT"] > marriagedate:
+                print(f"Indi id -> {childobj['INDI']}, Birth before marriage")
+                return False
+            else:
+                print(f'child with id {child} does not exist in indi list')
+                return False
+
+    print("User Story 8 Completed")
+    return True
