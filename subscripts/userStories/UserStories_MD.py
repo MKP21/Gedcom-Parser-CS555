936c28f62703d78c999dc309875e410b9276aa29
Mahir <mdhall@stevens.edu>
2020-02-29 17:11:59 -0500

    added userStory 14
    added userStory 14 and imported it to objectValidity

--- subscripts/userStories/UserStories_MD.py
+++ subscripts/userStories/UserStories_MD.py
@@ -16,10 +16,12 @@ def us4(indi, fam):
         else:
             marr = family['MARR']
             if marr == 'NA':
-                print(f"No marraiage date found in family with id {family['FAM']}")
+                print(
+                    f"No marraiage date found in family with id {family['FAM']}")
 
             if div < marr:
-                print(f"Family with id {family['FAM']} has divorce before marriage")
+                print(
+                    f"Family with id {family['FAM']} has divorce before marriage")
 
     # end of for loop
     print("User Story 4 Completed")
@@ -45,7 +47,33 @@ def us7(indi, fam):
             cd = datetime.now()
             no_of_days = cd - birth
             if no_of_days > timedelta(days=54750):
-                print(f"person with name {indv['NAME']} and id {indv['INDI']} is more than 150 years from birth")
+                print(
+                    f"person with name {indv['NAME']} and id {indv['INDI']} is more than 150 years from birth")
     # End of for loop
     print("User Story 7 Completed")
+    return True
+
+    # Multiple births <= 5
+
+
+def us14(indi, fam):
+    print("User Story 14 - Multiple births <= 5, Running")
+    for families in fam:
+        if families['CHIL'] == 'NA' or len(families['CHIL']) <= 5:
+            continue
+        else:
+            birthDates = []
+            count = 1
+            for child in families['CHIL']:
+                for indv in indi:
+                    if child == indv['INDI']:
+                        birthDates.append(indv['BIRT'])
+
+            for sib in range(len(birthDates) - 1):
+                if birthDates[sib] == birthDates[sib + 1]:
+                    count += 1
+            if count > 5:
+                print(f"family with id {families['FAM']}has more than 5 siblings who were born on the same date and time")
+    # End of for loop
+    print("User Story 14 Completed")
     return True
