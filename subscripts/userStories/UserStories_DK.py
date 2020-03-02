92d683597e96201dc3d9f22851d82c85e2429600
Deep kakadia <kakadiadeepus@gmail.com>
2020-03-02 15:13:02 -0500

    flag deep

--- subscripts/userStories/UserStories_DK.py
+++ subscripts/userStories/UserStories_DK.py
@@ -5,6 +5,7 @@ from datetime import timedelta
 # Birth before Marriage
 def us02(indi, fam, f):
     print("User Story 2 - Birth before Marriage, Running")
+    flag = True
     for families in fam:
         for person in indi:
             if families['HUSB'] == person['INDI'] or families['WIFE'] == person['INDI']:
@@ -12,22 +13,25 @@ def us02(indi, fam, f):
                     print("NO BIRTHDATE FOUND")
                     f.write(
                         f"Error: INDIVIDUAL: US02: { person['INDI']} {person['NAME']} birthdate not found \n")
-                    return False
+                    flag = False
                 m = person['BIRT']
                 if families['MARR'] < m:
                     print(
                         f" User Story 02  Error: { person['INDI']} {person['NAME']} were married before birth")
                     f.write(
                         f"Error: INDIVIDUAL: US02: { person['INDI']} {person['NAME']} were married before birth\n")
-                    return False
-    print("User Story 2 Completed")
-    return True
+                    flag = False
+    if flag:
+        print("User Story 2 Completed")
+    else:
+        return False
 
 # Birth before death of parents
 
 
 def us09(indi, fam, f):
     print("User Story 9 - Birth before death of parents, Running")
+    flag = True
     for family in fam:
         fatherdeath = 'NA'
         motherdeath = 'NA'
@@ -50,7 +54,9 @@ def us09(indi, fam, f):
                             f"Indi id -> {childobj['INDI']}, Birth before death of parents")
                         f.write(
                             f"Error: INDIVIDUAL: US09: { person['INDI']} {person['NAME']} Birth after death of parents\n")
-                        return False
-
-    print("User Story 9 Completed")
-    return True
+                        flag = False
+    if flag:
+        print("User Story 9 Completed")
+        return True
+    else:
+        return False
