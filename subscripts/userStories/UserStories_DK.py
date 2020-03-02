9d72444136163c786a9ef8a85cbe44890b1a7b3c
Deep kakadia <kakadiadeepus@gmail.com>
2020-03-02 16:57:34 -0500

    test update

--- subscripts/userStories/UserStories_DK.py
+++ subscripts/userStories/UserStories_DK.py
@@ -5,21 +5,25 @@ from datetime import timedelta
 # Birth before Marriage
 def us02(indi, fam, f):
     print("User Story 2 - Birth before Marriage, Running")
+    # SETTING 8
     flag = True
     for families in fam:
         for person in indi:
             if families['HUSB'] == person['INDI'] or families['WIFE'] == person['INDI']:
+                # CHECK IF PERSON HAS BIRTHDATE
                 if person['BIRT'] == 'NA':
                     print("NO BIRTHDATE FOUND")
                     f.write(
                         f"Error: INDIVIDUAL: US02: { person['INDI']} {person['NAME']} birthdate not found \n")
                     flag = False
+                # SETTING M = BIRTHDATE
                 m = person['BIRT']
+                # COMPARING MARRIAGE DATE TO BIRTHDATE
                 if families['MARR'] < m:
                     print(
-                        f" User Story 02  Error: { person['INDI']} {person['NAME']} were married before birth")
+                        f" User Story 02  Error: { person['INDI']} {person['NAME']} was born after marriage with {fam['WIFE']}")
                     f.write(
-                        f"Error: INDIVIDUAL: US02: { person['INDI']} {person['NAME']} were married before birth\n")
+                        f"Error: INDIVIDUAL: US02: { person['INDI']} {person['NAME']} was born after marriage {fam['WIFE']}\n")
                     flag = False
     if flag:
         print("User Story 2 Completed")
@@ -32,10 +36,12 @@ def us02(indi, fam, f):
 
 def us09(indi, fam, f):
     print("User Story 9 - Birth before death of parents, Running")
+    # SETTING FLAG
     flag = True
     for family in fam:
         fatherdeath = 'NA'
         motherdeath = 'NA'
+        # CHECKING FOR DEATH DATES OF MOTHER AND FATHER
         for person in indi:
             if family["HUSB"] == person["INDI"]:
                 if person["DEAT"] != 'NA':
@@ -49,12 +55,12 @@ def us09(indi, fam, f):
                 childobj = next(
                     (item for item in indi if item["INDI"] == child), False)
                 if childobj != "NA" and fatherdeath != "NA" and motherdeath != "NA":
-
+                    # checking if child is born after death of parents
                     if childobj["BIRT"] > motherdeath or childobj["BIRT"] > fatherdeath:
                         print(
-                            f"Indi id -> {childobj['INDI']}, Birth before death of parents")
+                            f"Indi id -> {childobj['INDI']}, Birth after death of parents")
                         f.write(
-                            f"Error: INDIVIDUAL: US09: { person['INDI']} {person['NAME']} Birth after death of parents\n")
+                            f"Error: INDIVIDUAL: US09: { childobj['INDI']} {childobj['NAME']} Birth after death of parents  \n")
                         flag = False
     if flag:
         print("User Story 9 Completed")
