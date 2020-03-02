ccb7c4f51174ea1fafbbfea8f0e4c25bd74a2f08
Aneri <anerishah.2710@gmail.com>
2020-03-02 13:20:50 -0500

    aneri user story

--- subscripts/userStories/UserStories_AS.py
+++ subscripts/userStories/UserStories_AS.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 
 # Marriage before death
-def us05(indi, fam):
+def us05(indi, fam, f):
     print("User Story 5 - Marriage before death, Running")
     for families in fam:
         for individuals in indi:
@@ -14,8 +14,9 @@ def us05(indi, fam):
                 if m != 'NA':
                     #checking for marriage date greater than the individual's death date
                     if families['MARR'] > m:
-                        print("Marriage before death")
+                        f.write(f"ERROR: INDIVIDUAL : US05 : : {individuals['INDI']} : Married {families['MARR']} after husband's ({individuals['INDI']}) death on {individuals['DEAT']} ")
             
+             # checking for wifi id is equal to individual id
             elif families['WIFE'] == individuals['INDI']:
                 # getting death date for wifi individual
                 n = individuals['DEAT']
@@ -23,7 +24,7 @@ def us05(indi, fam):
                 if n!= 'NA':
                     #checking for marriage date greater than the individual's death date
                     if families['MARR'] > n:
-                        print("Marriage before death")
+                        f.write(f"ERROR: INDIVIDUAL : US05 : : {individuals['INDI']} : Married {families['MARR']} after wifi's ({individuals['INDI']}) death on {individuals['DEAT']} ")
 
 
   
@@ -31,24 +32,29 @@ def us05(indi, fam):
     return True
 
 # Divorce before death
-def us06(indi, fam):
+def us06(indi, fam, f):
     print("User Story 6 - Divorce before death, Running")
     for families in fam:
         for individuals in indi:
             # checking for husband id is equal to individual id
             if families['HUSB'] == individuals['INDI']:
-                # getting death date for husband individual
+                # getting death date for husband's individual
                 m = individuals['DEAT']
+                # If individual's death date is not null and families divorce date is not null
                 if m != 'NA' and families['DIV'] !='NA':
+                    #checking for husband's death date less than the divorces date
                     if m < families['DIV']:
-                        print("Divorce before death")
+                        f.write(f"ERROR: FAMILY : US06 : : {individuals['INDI']} : Divorced {families['DIV']} after husband's ({individuals['INDI']}) death on {individuals['DEAT']} ")
             
+             # checking for wifi id is equal to individual id
             if families['WIFE'] == individuals['INDI']:
+                # getting death date for wifi's individual
                 n = individuals['DEAT']
+                # If individual's death date is not null and families divorce date is not null
                 if n!= 'NA' and families['DIV'] !='NA':
+                    #checking for wifi's death date less than the divorces date
                     if n < families['DIV']:
-                        print("Divorce before death")
-
+                        f.write(f"ERROR: FAMILY : US06 : : {individuals['INDI']} : Divorced {families['DIV']} after wifi's ({individuals['INDI']}) death on {individuals['DEAT']} ")
 
   
     print("User Story 6 Completed")
