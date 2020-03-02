ce1d43b51a7590f3303ed8311f6c0ed08db9428a
MKP21 <meetknpatel2@gmail.com>
2020-03-02 14:26:26 -0500

    added Unit Testing

--- test_cases/test.py
+++ test_cases/test.py
@@ -3,12 +3,13 @@ from subscripts.userStories.UserStories_AS import us05
 
 import unittest
 
-class TestCases(unittest.TestCase):
 
-    gedcom_error = open("My-Family-27-Jan-2020-330","r")
+class TestCases(unittest.TestCase):
+    gedcom_error = "../My-Family-27-Jan-2020-330.ged"
     d = fileParser(gedcom_error)
-    f = open("testing","w")
 
     def test_us05(self):
-        self.assertTrue(us05(d[0],d[1],f))
-        
+        f = open("test.txt", "w+")
+        value = us05(self.d[0], self.d[1], f)
+        f.close()
+        self.assertTrue(value)
