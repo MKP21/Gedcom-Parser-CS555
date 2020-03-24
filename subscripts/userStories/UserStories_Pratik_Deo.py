8d5bde19a9d121d84d3674450461e299a505a2d2
Deep kakadia <kakadiadeepus@gmail.com>
2020-03-24 14:48:35 -0400

    Pretty update

--- subscripts/userStories/UserStories_Pratik_Deo.py
+++ subscripts/userStories/UserStories_Pratik_Deo.py
@@ -1,52 +1,55 @@
 from datetime import date
 from subscripts.outputDisplay import calculateage
 
-
 today = date.today()
 dateList = []
 
 
 # User story 1 - all dates should be before current date
-def us01(indi, fam , f):
+def us01(indi, fam, f):
     print("US 01 - all dates should be before current date, Running")
-    lag = True 
+    lag = True
     for i in indi:
         # Checking death dates are before current dates and NA
         if str(i["DEAT"]) == "NA":
             pass
         elif i["DEAT"].date() > today:
             print("US 01 Error indi id ->" + str(i["INDI"]) + str(i["DEAT"]))
-            f.write(f"Error: INDIVIDUAL: US01: Date before Current Date" + str(i["INDI"]) + " "+str(i["DEAT"])+"\n")
-            lag =  False
+            f.write(f"Error: INDIVIDUAL: US01: Date before Current Date" + str(i["INDI"]) + " " + str(i["DEAT"]) + "\n")
+            lag = False
 
         # Checking for Birth dates are before current Date
         if str(i["BIRT"]) == "NA":
             pass
         elif i["BIRT"].date() > today:
             print("These dates are after the current date: " + str(i["NAME"]) + str(i["BIRT"].date()))
-            f.write(f"Error: INDIVIDUAL: US01: Birth date after current date" + str(i["INDI"])+ " "+str(i["NAME"]) +" "+ str(i["BIRT"].date()) +"\n")
+            f.write(f"Error: INDIVIDUAL: US01: Birth date after current date" + str(i["INDI"]) + " " + str(
+                i["NAME"]) + " " + str(i["BIRT"].date()) + "\n")
             lag = False
 
     for j in fam:
 
         if j["MARR"].date() > today:
-            print("Error: INDIVIDUAL: US01: Marriage date after current date " + str(j["MARR"].date()) + " " + str(today))
+            print(
+                "Error: INDIVIDUAL: US01: Marriage date after current date " + str(j["MARR"].date()) + " " + str(today))
             lag = False
-            f.write(f"Error: INDIVIDUAL: US01: Marriage date after current date " + str(j["MARR"].date()) + " " + str(today)+"\n")
+            f.write(f"Error: INDIVIDUAL: US01: Marriage date after current date " + str(j["MARR"].date()) + " " + str(
+                today) + "\n")
 
         if str(j["DIV"]) == "NA":
             pass
         elif j["DIV"].date() > today:
-            print("Error: INDIVIDUAL: US01: Divorce date after current date " + str(j["DIV"].date()) +" " + str(today))
+            print("Error: INDIVIDUAL: US01: Divorce date after current date " + str(j["DIV"].date()) + " " + str(today))
             lag = False
-            f.write(f"Error: INDIVIDUAL: US01: Divorce date after current date " + str(j["DIV"].date()) +" " + str(today)+"\n")
-    
-    if(lag):
+            f.write(f"Error: INDIVIDUAL: US01: Divorce date after current date " + str(j["DIV"].date()) + " " + str(
+                today) + "\n")
+
+    if (lag):
         print("US 01 completed")
         return True
-    else: 
+    else:
         return False
-    
+
 
 # User story 10 - Marriage should be after 14 years of age
 def us10(indi, fam, f):
@@ -61,8 +64,9 @@ def us10(indi, fam, f):
                     pass
                 else:
                     print("US 10 Error indi id ->" + str(i["INDI"]) + str(j["MARR"]))
-                    f.write("Error: INDIVIDUAL: US10: Too young for marriage " + str(i["INDI"]) +" "+ str(j["MARR"])+"\n")
-                    #return False list2.append("false")
+                    f.write("Error: INDIVIDUAL: US10: Too young for marriage " + str(i["INDI"]) + " " + str(
+                        j["MARR"]) + "\n")
+                    # return False list2.append("false")
                     flag = False
 
             if i["INDI"] == j["HUSB"]:
@@ -71,39 +75,44 @@ def us10(indi, fam, f):
                 if age > 14:
                     pass
                 else:
-                    print("US 10 Error indi id ->" + str(i["INDI"]) + str(j["MARR"])) 
-                    f.write("Error: INDIVIDUAL: US10: Too young for marriage " + str(i["INDI"]) +" "+ str(j["MARR"])+"\n")
-                    #return False
-                      
-                    flag = False              
-    
-    if(flag):
+                    print("US 10 Error indi id ->" + str(i["INDI"]) + str(j["MARR"]))
+                    f.write("Error: INDIVIDUAL: US10: Too young for marriage " + str(i["INDI"]) + " " + str(
+                        j["MARR"]) + "\n")
+                    # return False
+
+                    flag = False
+
+    if (flag):
         return True
-        print("US 10 completed")
+        print("US 10 completed \n \n")
+        print("---------------- SPRINT @ COMPLETED -------------------\n \n")
     else:
         return False
 
+
 # User story 15 - Fewer than 15 siblings
-def us15(indi, fam,f):
+def us15(indi, fam, f):
     print("US 15 - Fewer than 15 sublings - Running")
     flag = True
     list_of_siblings = []
     for i in fam:
-        if(len(i["CHIL"]) >= 15):
+        if (len(i["CHIL"]) >= 15):
             list_of_siblings.append(i["FAM"])
 
-    if(len(list_of_siblings) == 0):
+    if (len(list_of_siblings) == 0):
         pass
     else:
         print("Error: FAM: US15: More than 15 siblings in a family" + str(i["FAM"]))
-        f.write("Error: FAM: US15: More than 15 siblings in a family" + str(i["FAM"])+ "\n")
+        f.write("Error: FAM: US15: More than 15 siblings in a family" + str(i["FAM"]) + "\n")
         flag = False
-    if(flag):
+    if (flag):
         print("US 15 completed")
         return True
-    else: return False
+    else:
+        return False
 
-#User Story 16
+
+# User Story 16
 
 
 def us16(indi, fam, f):
@@ -112,65 +121,69 @@ def us16(indi, fam, f):
     for j in fam:
         names = []
         names.append(getLastNamebyId(indi, j["HUSB"]))
-        if(j["CHIL"] != []):
+        if (j["CHIL"] != []):
             for h in j["CHIL"]:
-                if(getSexByid(indi, h ) == 'M'):
+                if (getSexByid(indi, h) == 'M'):
                     names.append(getLastNamebyId(indi, h))
 
-        if(len(set(names)) != 1):
+        if (len(set(names)) != 1):
             print("Error: FAM: US 16: Single family has two or more last name" + str(j["FAM"]))
-            f.write("Error: FAM: US 16: Single family has two or more last name " + str(j["FAM"])+"\n")
+            f.write("Error: FAM: US 16: Single family has two or more last name " + str(j["FAM"]) + "\n")
             flag = False
         elif (len(set(names)) == 1):
             pass
 
-    if(flag):
+    if (flag):
         return True
         print("US 16 completed")
     else:
         return False
 
 
-#Helper functions
+# Helper functions
 def getLastNamebyId(indi, Id):
     for i in indi:
-        list1= []
-        if(i["INDI"] == Id):
+        list1 = []
+        if (i["INDI"] == Id):
             s = i["NAME"]
-            #print(s)
+            # print(s)
             list1 = s.split()
             return list1[1]
+
 
 def getSexByid(indi, Id):
     sex = ""
     for i in indi:
-        if(i["INDI"] == Id):
+        if (i["INDI"] == Id):
             sex = i["SEX"]
-            
+
     return sex
+
 
 def getAgeById(indi, Id):
     age = 0
     for i in indi:
-        if(i["INDI"] == Id ):
+        if (i["INDI"] == Id):
             age = calculateage(i["BIRT"], i["DEAT"])
-    
+
     return age
+
 
 def getAliveById(indi, Id):
     alive = True
     for i in indi:
-        if(i["DEAT"] == "NA"):
+        if (i["DEAT"] == "NA"):
             alive = True
-        else: 
+        else:
             alive = False
-    
+
     return alive
+
 
 def getNamebyId(indi, Id):
     name = " "
     for i in indi:
-        if(i["INDI"] == Id):
+        if (i["INDI"] == Id):
             name = i["NAME"]
 
     return name
